39873776f0150ce64602f86eed07524c482e5ee7
Sweth Chandramouli <schandramouli@shutterstock.com>
2017-12-06 00:12:45 -0600

    6a

---
+++ 2017/06a/redist.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use DDP;
+
+while (<>) {
+    my ($input, $expected) = /([^#]+)(?:\s?#\s?)?(\d+)?/;
+    my @registers = split(' ', $input);
+    my $maxRegisterIndex = $#registers;
+    my %seen;
+    my $count = 0;
+    while (not $seen{join('.', @registers)}) {
+        $seen{join('.', @registers)}++;
+        $count++;
+        do { print STDERR "\n$count:\n"; p(@registers); p(%seen);} if $ENV{'DEBUG'};
+        my $indexOfFirstLargest = 0;
+        $registers[$indexOfFirstLargest] > $registers[$_]
+            or $indexOfFirstLargest = $_
+            for reverse (0 .. $#registers);
+        my $valueToDistribute = $registers[my $currentIndex = $indexOfFirstLargest];
+        @registers[$indexOfFirstLargest] = 0;
+        print STDERR "Reset Index $indexOfFirstLargest to $registers[$currentIndex]\n"
+            if $ENV{'DEBUG'};
+        for (1 .. $valueToDistribute) {
+            $currentIndex = 0 if (++$currentIndex > $maxRegisterIndex);
+            $registers[$currentIndex]++;
+            print STDERR "Set Index $currentIndex to $registers[$currentIndex]\n"
+                if $ENV{'DEBUG'};
+        }
+        p(@registers) if $ENV{'DEBUG'};
+    }
+    print "\nInput: $input\n";
+    print "Expected: $expected\n" if defined ($expected);
+    print "Calcuated: $count\n";
+}
